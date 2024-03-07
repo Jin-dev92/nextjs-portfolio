94477bfc0e49bfbdf76fd0aa0a16c9f49bf47401
Jin <kpasd002@gmail.com>
2024-03-07 15:49:36 +0900

    fix. 빌드 에러 제거

--- api/service/dto/get-current-weather.dto.ts
+++ api/service/dto/get-current-weather.dto.ts
@@ -1,8 +1,7 @@
-import { DataTypeEnum } from "../../../components/data-chart/card/constant/dataType.ts";
 
 export interface GetCurrentWeatherDto {
   pageNo: number;
-  dataType: DataTypeEnum;
+  dataType: string ;
   numOfRows: number;
   base_date: string;
   base_time: number[];
