cfd1256b352e640b5dd6b99794300760e9b9123e
Jin <kpasd002@gmail.com>
2024-03-11 17:33:50 +0900

    feat. QueryClient Provider 제작

--- store/user.store.ts
+++ store/user.store.ts
@@ -4,5 +4,5 @@ import { IUserStore } from '@/store/type/interface/user-store.interface';
 
 export const userStore = create<IUserStore>(set => ({
   currentUser: null,
-  setCurrentUser: user => set(state => ({ currentUser: state.currentUser }))
+  setCurrentUser: user => set(state => ({ currentUser: user }))
 }));
